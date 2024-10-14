e389271076f35a10d395c2e2d68ff0c40fb04e47
Aymen Shoteri <aymenshoteri@gmail.com>
2024-10-14 14:45:33 -0400

    got the mouse and arrow to work, title has animation issues

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,18 +25,18 @@ const Home = () => {
     });
 
     gsap.from(textTitle.current, {
-      y: '50%', // Move from the right side
+      y: 80,
       opacity: 0,
       duration: 3.5,
       delay: 1.5,
-      ease: 'power2.out',
+      ease: 'power3.out',
     });
 
     gsap.to(point.current, {
-      y: '100%',           // Move down by 50%
+      y: '60px',        // Move down by 50%
       opacity: 0,         // Optional: if you want opacity to fade out
-      duration: 1.5,      // Duration of each down/up cycle
-      ease: 'power0.out', // Ease function for smooth motion
+      duration: 1.0,      // Duration of each down/up cycle
+      ease: 'sine.out', // Ease function for smooth motion
       repeat: -1,         // Repeat infinitely
       repeatDelay: 0.5,   // Optional: Delay between each cycle (you can change or remove this)
     });
@@ -50,9 +50,9 @@ const Home = () => {
       <img src="public/logo.png" className="text-5xl" alt="Logo" />
       <h2 ref={textTitle} className="text-light p-10 tracking-widest text-4xl" style={{ letterSpacing: '0.4em' }}>B R O T H E R S Я E T R E A T</h2> 
       </div>
-      <div>
+      <div className='absolute bottom-[10vh] flex items-center justify-center flex-col'>
         <img src='public/mouse.png' className="w-16" alt="Mouse" />
-        <img ref={point} src="public/arrow.png" className="w-16" alt="Arrow" />
+        <img ref={point} src="public/arrow.png" className="w-12 m-2" alt="Arrow" />
       </div>
     </div>
   );
