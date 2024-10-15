4615b162537d59a32a38b030cf15035bbb62663a
Aymen Shoteri <aymenshoteri@gmail.com>
2024-10-15 13:06:17 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-dark text-white p-4 shadow-lg border-b-2 border-light">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">R2R</h1>
+    <nav className="fixed top-0 left-0 z-999 w-full bg-dark text-white p-4 shadow-lg border-b-2 border-light">
+      <div className="flex justify-between items-center w-full px-4">
+        <img src='public/logo.png' className="w-10" alt="Logo" />
         <ul className="flex space-x-4">
           <li><Link to="/" className="hover:underline">Home</Link></li>
           <li><Link to="/about" className="hover:underline">About</Link></li>
