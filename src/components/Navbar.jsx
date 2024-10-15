f6f71f21137f7018c388b5659f10cf3d5bfebcb2
SamiN <saminassif01@gmail.com>
2024-10-14 22:42:46 -0400

    -made the TEMPLATE for contact, no specifics added. -Layout finished. -Made z-index changes on the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-dark text-white p-4 shadow-lg border-b-2 border-light">
+    //changed z-index on the navbar enough to overlap everything
+    <nav className="fixed top-0 left-0 w-full bg-dark text-white p-4 shadow-lg border-b-2 border-light z-50">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">R2R</h1>
         <ul className="flex space-x-4">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,26 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
       colors: {
         dark: '#0b0b0b',
         light: '#fefefe',
+      },
+      scale: {
+        '110': '1.1',
+        '125': '1.25',
+      },
+      animation: {
+        fadeIn: 'fadeIn 2s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
       },
     },
   },
